962ab0fa061c871def6b5b811de43fb7480624f5
katger <k4tger@gmail.com>
2019-07-03 12:37:26 +0300

    Added method to get the response code

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.common.exceptions import NoSuchElementException
-
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import math
 
 class BasePage(object):
     def __init__(self, browser, url, timeout = 10):
@@ -18,3 +21,17 @@ class BasePage(object):
         except NoSuchElementException:
             return False
         return True
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            WebDriverWait(self.browser, 3).until(EC.alert_is_present())
+            alert = self.browser.switch_to.alert
+            print("Your code: {}".format(alert.text))
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
