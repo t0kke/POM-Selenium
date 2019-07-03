c2bc323a2b761e30537974adf6331a4c871194cd
katger <k4tger@gmail.com>
2019-07-03 12:33:24 +0300

    Added locators for ProductPage

--- pages/locators.py
+++ pages/locators.py
@@ -6,5 +6,14 @@ class MainPageLocators(object):
 
 
 class LoginPageLocators(object):
-    REG_FORM = (By.CSS_SELECTOR, "#id_registration-email")
-    LOGIN_FORM = (By.CSS_SELECTOR, "#id_login-username")
+    REG_FORM = (By.CSS_SELECTOR, "id_registration-email")
+    LOGIN_FORM = (By.CSS_SELECTOR, "id_login-username")
+
+
+class ProductPageLocators(object):
+    ADD_TO_CART = (By.CSS_SELECTOR, "button.btn-add-to-basket")
+    TITLE_BOOK  = (By.CSS_SELECTOR, "div.product_main > h1")
+    PRICE_BOOK = (By.CSS_SELECTOR, "div.product_main > p.price_color")
+    ALERT_TITLE_BOOK = (By.XPATH, '//*[@id="messages"]/div[1]/div/strong')
+    ALERT_PRICE_BOOK = (By.XPATH, '//*[@id="messages"]/div[3]/div/p[1]/strong')
+
