05ddf1d91ae649857b56c9a749d1da8467ea9275
katger <k4tger@gmail.com>
2019-07-03 12:37:47 +0300

    transition between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,7 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert "login" in str(self.url), "Link does not contain login page"
+        assert "login" in str(self.browser.current_url), "Link does not contain login page"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
