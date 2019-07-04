18b0da4b1e3421d66eb6a881689e65c097346649
katger <k4tger@gmail.com>
2019-07-04 15:53:07 +0300

    add test case

--- test_product_page.py
+++ test_product_page.py
@@ -2,24 +2,22 @@ from pages.product_page import ProductPage
 import time
 import pytest
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-def test_guest_can_add_product_to_cart(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+@pytest.mark.base
+def test_guest_can_add_product_to_cart(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"
     page = ProductPage(browser, link)
     page.open()
     page.add_product_to_cart()
     page.solve_quiz_and_get_code()
-    time.sleep(2)
+    time.sleep(3)
     page.should_be_product_name_match()
     page.should_be_price_product_corresponds_to_price_in_basket()
 
+
+@pytest.mark.subtest
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
