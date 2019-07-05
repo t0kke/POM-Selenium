64fca1f24cb0356e7cc33b99cca51c0befe1a934
katger <k4tger@gmail.com>
2019-07-05 15:42:24 +0300

    Code Style

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,10 +12,12 @@ class ProductPage(BasePage):
 
     def should_be_product_name_match(self):
         assert self.browser.find_element(*ProductPageLocators.TITLE_BOOK).text == self.browser.find_element(*ProductPageLocators.ALERT_TITLE_BOOK).text, \
-            "Book title {} does not match {}".format(self.browser.find_element(*ProductPageLocators.TITLE_BOOK).text, self.browser.find_element(*ProductPageLocators.ALERT_TITLE_BOOK).text)
+            "Book title {} does not match {}".format(self.browser.find_element(*ProductPageLocators.TITLE_BOOK).text,
+                                                     self.browser.find_element(*ProductPageLocators.ALERT_TITLE_BOOK).text)
 
     def should_be_price_product_corresponds_to_price_in_basket(self):
-        assert self.browser.find_element(*ProductPageLocators.PRICE_BOOK).text == self.browser.find_element(*ProductPageLocators.ALERT_PRICE_BOOK).text, "Product price does not match the price in the basket"
+        assert self.browser.find_element(*ProductPageLocators.PRICE_BOOK).text == self.browser.find_element(*ProductPageLocators.ALERT_PRICE_BOOK).text, \
+            "Product price does not match the price in the basket"
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(*ProductPageLocators.ALERT_TITLE_BOOK), \
