4a6ead5991cc2851c54156276c7062a1595d0394
katger <k4tger@gmail.com>
2019-07-05 12:42:11 +0300

    Add test for basket page

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,8 @@
 from pages.product_page import ProductPage
+from pages.basket_page import BasketPage
 import time
-import pytest
 
 
-@pytest.mark.base
 def test_guest_can_add_product_to_cart(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6"
     page = ProductPage(browser, link)
@@ -15,7 +14,6 @@ def test_guest_can_add_product_to_cart(browser):
     page.should_be_price_product_corresponds_to_price_in_basket()
 
 
-@pytest.mark.subtest
 def test_guest_cant_see_success_message(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
@@ -29,8 +27,22 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_cart_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_product_in_basket()
+    basket_page.should_be_message_basket_is_empty()
+
+
+
