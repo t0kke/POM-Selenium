0c0aa8d7abf47e6fc0845123f243bba66e9f283c
katger <k4tger@gmail.com>
2019-07-05 17:05:24 +0300

    Authorization verification method added

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -36,6 +36,10 @@ class BasePage(object):
         link = self.browser.find_element(*BasePageLocators.BASKET_LINK)
         link.click()
 
+    def should_be_authorized_user(self):
+        assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not presented," \
+                                                                     " probably unauthorised user"
+
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
